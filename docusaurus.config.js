45a0826d9003741d64257a501a1ceac024739794
Chris Troutner <chris.troutner@gmail.com>
2024-09-18 17:22:08 -0700

    Added new feature images to homepage

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,12 +8,12 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'OpenSourceMetalWallet.com',
+  tagline: 'Paper Wallets & Laser Engraved Metal Wallets',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://opensourcemetalwallet.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -4,32 +4,32 @@ import styles from './styles.module.css';
 
 const FeatureList = [
   {
-    title: 'Easy to Use',
-    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    title: 'Learn More',
+    Svg: require('@site/static/img/plastic-wallet-thubnail.svg').default,
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        This video was the inspiration for this site. It describes how inexpensive
+        laser engravers can be used to make durable 'paper wallets' for families,
+        friends, and the crypto-curious.
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
-    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    title: 'More Videos',
+    Svg: require('@site/static/img/yt_logo_rgb_light.svg').default,
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        Learn about the advantages and shortcomings of paper wallets, and how
+        they work.
       </>
     ),
   },
   {
-    title: 'Powered by React',
-    Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
+    title: 'Buy Metal Wallets',
+    Svg: require('@site/static/img/buy-now.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Order your own 'paper wallets' engraved onto stainless steel business cards.
       </>
     ),
   },
