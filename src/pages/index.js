b2d2d5d730f69bf401497ac3c12b3e69c37a70a0
Chris Troutner <chris.troutner@gmail.com>
2024-10-15 16:57:49 -0300

    Converted homepage component

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,7 @@ import HomepageFeatures from '@site/src/components/HomepageFeatures';
 
 import Heading from '@theme/Heading';
 import styles from './index.module.css';
+import stylesFeature from './feature-styles.module.css';
 
 import Translate, {translate} from '@docusaurus/Translate';
 
@@ -18,7 +19,7 @@ function HomepageHeader() {
           {siteConfig.title}
         </Heading>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <img src="/img/metal-wallet.jpeg" style={{width: '400px'}} />
+        <img src="./img/metal-wallet.jpeg" style={{width: '400px'}} />
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
@@ -38,9 +39,62 @@ export default function Home() {
       title={`${siteConfig.title}`}
       description="Description will go into a meta tag in <head />">
       <HomepageHeader />
+
       <main>
-        <HomepageFeatures />
+        <section className={stylesFeature.features}>
+          <div className="container">
+            <div className="row">
+
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <Link to={"https://youtu.be/3qn0mmfwlBQ?si=FxV4_vQlaPek0RNK"}>
+                    <img className={stylesFeature.featureSvg} src="./img/img2.jpeg" />
+                  </Link>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <Heading as="h3">{'Plastic Wallets'}</Heading>
+                  <p>
+                    This video was the inspiration for this site. It describes how inexpensive
+                    laser engravers can be used to make durable 'paper wallets' for families,
+                    friends, and the crypto-curious.
+                  </p>
+                </div>
+              </div>
+
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <Link to={"/docs/intro"}>
+                    <img className={stylesFeature.featureSvg} src="./img/laser-enrgraver.png" />
+                  </Link>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <Heading as="h3">{'Learn More!'}</Heading>
+                  <p>
+                    Learn about paper wallets, the shortcomings and advantages, and how to
+                    make your own with a desktop printer or a laser engraver.
+                  </p>
+                </div>
+              </div>
+
+              <div className={clsx('col col--4')}>
+                <div className="text--center">
+                  <Link to={"https://store.opensourcemetalwallet.com"}>
+                    <img style={{width: '200px', marginTop: '135px'}} src="./img/buy-now.png" />
+                  </Link>
+                </div>
+                <div className="text--center padding-horiz--md">
+                  <Heading as="h3">{'Buy Metal Wallets'}</Heading>
+                  <p>
+                    Order your own 'paper wallets' engraved onto stainless steel business cards.
+                  </p>
+                </div>
+              </div>
+
+            </div>
+          </div>
+        </section>
       </main>
+
     </Layout>
   );
 }
